a89e545675c049eb74c95721d3bb31c8d69f71f7
ndren <andreien@proton.me>
2024-01-23 03:45:22 +0000

    fix _wget_download security problem

--- python/paddle/utils/download.py
+++ python/paddle/utils/download.py
@@ -15,6 +15,7 @@
 import hashlib
 import os
 import os.path as osp
+import shlex
 import shutil
 import subprocess
 import sys
@@ -198,7 +199,8 @@ def _get_download(url, fullname):
 
 def _wget_download(url, fullname):
     # using wget to download url
-    tmp_fullname = fullname + "_tmp"
+    tmp_fullname = shlex.quote(fullname + "_tmp")
+    url = shlex.quote(url)
     # –user-agent
     command = f'wget -O {tmp_fullname} -t {DOWNLOAD_RETRY_LIMIT} {url}'
     subprc = subprocess.Popen(
